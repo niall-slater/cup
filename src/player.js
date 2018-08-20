4c0b5d5c612f1db555da4970eca1ce9b69def790
niall-slater <niallslater@gmail.com>
2018-08-20 15:21:08 +0100

    Separated out player class, installed Grunt

---
+++ src/player.js
@@ -0,0 +1,59 @@
+class Player extends Phaser.Sprite {
+    
+    constructor(game, x, y) {
+        super(game, 0, 0);
+         
+        Phaser.Sprite.call(this, game, x, y, 'characters_test');
+		
+        
+        this.anchor.setTo(0.5, 0.5);
+		
+		this.animSpeed = 4;
+		this.moveSpeed = 100;
+		
+		/*
+        this.anim_idle = this.animations.add('anim_idle', [0]);
+        this.anim_walk = this.animations.add('anim_walk', [0,1]);
+        this.anim_interact = this.animations.add('anim_interact', [2,3]);
+		*/
+		
+        game.add.existing(this);
+        game.physics.arcade.enable(this);
+		this.body.setSize(14, 14, 0, 0);
+    	this.body.drag = 50;
+    	this.body.collideWorldBounds = true;
+		game.camera.follow(this);
+		
+		//this.animations.play('anim_walk', this.animSpeed, true);
+    }
+    
+    update() {
+		
+    	this.body.velocity.x = 0;
+    	this.body.velocity.y = 0;
+		
+		if (cursors.left.isDown)
+		{
+			this.body.velocity.x = -this.moveSpeed;
+		}
+		else if (cursors.right.isDown)
+		{
+			this.body.velocity.x = this.moveSpeed;
+		}
+
+		if (cursors.up.isDown)
+		{
+			this.body.velocity.y = -this.moveSpeed;
+		}
+		else if (cursors.down.isDown)
+		{
+			this.body.velocity.y = this.moveSpeed;
+		}
+		
+    }
+	
+    die() {
+        this.destroy();
+    }
+	
+};
--- src/state_play.js
+++ src/state_play.js
@@ -5,13 +5,14 @@ let cursors;
 var playState = {
 	
 	world: {},
-	cameraScale: 1,
+	cameraScale: 2,
 	
 	init: function(mapIndexes) {
 		this.world.mapIndexes = mapIndexes;
 	},
 	
 	preload: function() {
+		
 	},
 	
 	create: function() {
@@ -20,7 +21,27 @@ var playState = {
 		game.time.desiredFps = 30;
 		
 		//Create map
+		this.createMap();
 		
+		this.player = new Player(game, 32, 32);
+    	cursors = game.input.keyboard.createCursorKeys();
+		
+		//Display FPS
+		game.time.advancedTiming = true;
+	},
+	
+	update: function() {
+		
+    	game.physics.arcade.collide(this.player, this.map.layer2);
+
+	},
+	
+	render: function() {
+		game.debug.text(game.time.fps, 5, 15, '#fff');
+	},
+	
+	createMap: function() {
+	
 		let currentChunkCoords = {x:0, y:2};
 		
 		//This x and y is the wrong way round - TODO: fix it! leaving it here for testing though
@@ -47,53 +68,7 @@ var playState = {
 		
 		this.map.setCollisionByExclusion([], true, 'middle', false);
 		//map.setCollisionBetween(0, 999, true, 'middle', false);
-
-		this.player = game.add.sprite(12, 12, 'characters_test');
-		game.physics.enable(this.player);
-		this.player.body.linearDamping = 1;
-		this.player.anchor.set(0.5);
-		this.player.body.setSize(14, 14, 0, 0);
-		this.player.walkSpeed = 100;
-    	this.player.body.drag = 50;
-    	this.player.body.collideWorldBounds = true;
-		game.camera.follow(this.player);
-    	cursors = game.input.keyboard.createCursorKeys();
-		
-		//Display FPS
-		game.time.advancedTiming = true;
-	},
 	
-	update: function() {
-		
-    	game.physics.arcade.collide(this.player, this.map.layer2);
-		
-    	this.player.body.velocity.x = 0;
-    	this.player.body.velocity.y = 0;
-		
-		if (cursors.left.isDown)
-		{
-			this.player.body.velocity.x = -this.player.walkSpeed;
-		}
-		else if (cursors.right.isDown)
-		{
-			this.player.body.velocity.x = this.player.walkSpeed;
-		}
-
-		if (cursors.up.isDown)
-		{
-			this.player.body.velocity.y = -this.player.walkSpeed;
-		}
-		else if (cursors.down.isDown)
-		{
-			this.player.body.velocity.y = this.player.walkSpeed;
-		}
-
-	},
-	
-	render: function() {
-		game.camera.scale.x = this.cameraScale;
-		game.camera.scale.y = this.cameraScale;
-		game.debug.text(game.time.fps, 5, 15, '#fff');
 	},
 	
 	goToChunk: function(chunkX, chunkY, playerX, playerY) {
@@ -107,16 +82,16 @@ var playState = {
 		
 		this.world.currentChunk.addTilesetImage('roguelike_general', 'tiles_roguelike');
 		
-		let map = this.world.currentChunk;
+		let currentMap = this.world.currentChunk;
 		
-		let layer0 = map.createLayer('under');
-		let layer1 = map.createLayer('under-overlay');
-		let layer2 = map.createLayer('middle');
-		let layer3 = map.createLayer('over');
+		let layer0 = currentMap.createLayer('under');
+		let layer1 = currentMap.createLayer('under-overlay');
+		let layer2 = currentMap.createLayer('middle');
+		let layer3 = currentMap.createLayer('over');
 		
 		layer0.resizeWorld();
 		
-		map.setCollisionBetween(1, 100, true, 'middle');
+		currentMap.setCollisionBetween(1, 100, true, 'middle');
 	}
 	
 };
