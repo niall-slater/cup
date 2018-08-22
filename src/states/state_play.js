c5685ae6baf82a624a16d0b716c148c1c66906ab
Niall Slater <niallslater@gmail.com>
2018-08-22 16:15:30 +0100

    Loading objects from Tiled object layer

--- src/states/state_play.js
+++ src/states/state_play.js
@@ -30,15 +30,8 @@ var playState = {
         this.groupEffects = game.add.group();
         
 		this.player = new Player(game, 296, 250);
-		
         this.groupActors.add(this.player);
         
-		this.critter = new Critter(game, 240, 100);
-		this.critter.moveTo(this.player.x, this.player.y);
-        this.groupActors.add(this.critter);
-		this.speaker = new Speaker(game, 260, 100, 'I lost my cup. :(');
-        this.groupActors.add(this.speaker);
-		
 		//Create map
 		this.createMap();
     	cursors = game.input.keyboard.createCursorKeys();
@@ -48,7 +41,7 @@ var playState = {
 	},
 	
 	update: function() {
-		
+        
     	this.handleObjectCollision();
         this.handleWorldCollision();
 	},
@@ -67,21 +60,33 @@ var playState = {
 	
 	setUpChunk: function(chunkID) {
 		
-		//console.log('Setting up chunk ' + this.currentChunkCoords.x + ',' + this.currentChunkCoords.y + ' which is a chunk of variant ' + chunkID);
-		
         this.world.currentChunk = game.add.tilemap('chunk_' + chunkID);
-		
 		this.world.currentChunk.addTilesetImage('roguelike_general', 'tiles_roguelike');
 		
+        // Build the tilemap from the cached Tiled JSON file
 		this.currentLayers.push(this.world.currentChunk.createLayer('under'));
 		this.currentLayers.push(this.world.currentChunk.createLayer('under-overlay'));
 		this.currentLayers.push(this.world.currentChunk.createLayer('middle'));
 		this.currentLayers.push(this.world.currentChunk.createLayer('over'));
-		this.currentLayers[0].resizeWorld();
+        
+        let objects = this.world.currentChunk.objects.objects;
+        
+        objects.map(this.createFromObject);
 
-		//layer2.debug = true;
-		
+		this.currentLayers[0].resizeWorld();
+        
+        // Set the middle layer to be the collision layer        
 		this.world.currentChunk.setCollisionByExclusion([], true, 'middle', false);
+		
+        
+        /* TODO: Critters, creatures and other bits should be generated from JSON data */
+        
+		this.critter = new Critter(game, 240, 100);
+		this.critter.moveTo(this.player.x, this.player.y);
+        this.groupActors.add(this.critter);
+		
+        /* Arrange render layers for tiles and objects */
+        
 		game.world.bringToTop(this.groupActors);
 		this.currentLayers[2].bringToTop();
 		this.currentLayers[3].bringToTop();
@@ -136,6 +141,15 @@ var playState = {
 			if (this.currentChunkCoords.y < 3)
 				this.goToChunk(this.currentChunkCoords.x, this.currentChunkCoords.y + 1, this.player.x, 18);
 		}
+    },
+    
+    createFromObject: function(item, index) {
+        //Create an entity based on data from a Tiled JSON object
+        switch(item.type) {
+            case 'speaker': {
+                playState.groupActors.add(new Speaker(game, item.x, item.y, item.properties.phrase));
+            }
+        }
     }
 	
 };
