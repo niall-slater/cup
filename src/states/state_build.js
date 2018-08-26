49165d3dbd2ba5bf3820b3a259f4ff0327a20c99
niall-slater <niallslater@gmail.com>
2018-08-26 11:36:00 +0100

    Refactored build state to correctly use chunk subtypes

--- src/states/state_build.js
+++ src/states/state_build.js
@@ -6,37 +6,28 @@ var MAP_NUMBEROFCHUNKSUBTYPES = 1;
 
 var buildState = {
 	
-	preload: function() {
-        
-		//Assemble a map using 16 random pre-loaded chunks
-		/* Build a 2D array of indexes, that's all, and use them to choose which chunk to load on scene change */
-		
-	},
-	
 	create: function() {
 		
-		let map = [];
+		//Assemble a map using 16 random pre-loaded chunks
+		/* Build a 2D array of indexes and use them to choose which chunk to load on scene change */
 		
-		//TODO: Rewrite this to randomise the subtype but not the type
-		for (let x = 0; x < MAP_SIZE; x++) {
-			let column = [];
-			for (let y = 0; y < MAP_SIZE; y++) {
-				//Load a test tilemap chunk for now
-				let index = Math.floor(Math.random() * MAP_NUMBEROFCHUNKTYPES) + '_' + Math.floor(Math.random() * MAP_NUMBEROFCHUNKSUBTYPES);
-				column.push(index);
-			}
-			map.push(column);
-		}
-		
-		//This x and y is the wrong way round - TODO: fix it! leaving it here for testing though
-		map = [
+		let map = [
 			['4_0',		'1_0',	'3_0',	'2_0'],
 			['14_0',	'0_0',	'6_0',	'5_0'],
 			['15_0',	'8_0',	'10_0',	'11_0'],
 			['9_0',		'7_0',	'12_0',	'13_0']
 		];
 		
-		console.log(map);
+		
+		for (let x = 0; x < MAP_SIZE; x++) {
+			let column = [];
+			for (let y = 0; y < MAP_SIZE; y++) {
+				let index = Math.floor(Math.floor(Math.random() * MAP_NUMBEROFCHUNKSUBTYPES));
+				let string = map[x][y];
+				string = string.replace('_0', '_' + index);
+				map[x][y] = string;
+			}
+		}
 		
 		game.state.start('state_play', true, false, map);
 	}
